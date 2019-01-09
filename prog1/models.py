92d7e489d75f4d79990f16586d88f89b885bdbc4
v.samarets <sam_vadim@ua.fm>
2019-01-09 10:20:38 +0200

    Proba old view comment

--- prog1/models.py
+++ prog1/models.py
@@ -5,9 +5,13 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField(null=True)
-    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    age = models.IntegerField(null=True, verbose_name="Возраст")
+    surname = models.CharField(max_length=100, verbose_name="Фамилия")
 
     def __str__(self):
         return self.name+' '+self.surname
+
+    class Meta:
+        verbose_name = "Человека"
+        verbose_name_plural = "Люди"
